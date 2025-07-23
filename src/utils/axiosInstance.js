241a067878eacc64bc3d1c4ac81abcc0882f3ce8
Danish Surah <mr.surah12345@gmail.com>
2025-07-23 20:33:26 +0530

    final commit

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://personal-diary-backend-18qk.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
